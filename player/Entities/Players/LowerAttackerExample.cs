521890ee5f2ec64005e4ca34146bb48667f519cf
Ira <irishka.kr@gmail.com>
2018-06-14 09:16:02 +0300

    bug fix - upeer bound r

--- player/Entities/Players/LowerAttackerExample.cs
+++ player/Entities/Players/LowerAttackerExample.cs
@@ -51,7 +51,7 @@ namespace RoboCup
                         }
                         else
                         {
-                            if (player.Pos.Value.X > 10 || player.Pos.Value.Y > 5)
+                            if (player.Pos.Value.X > 10 || player.Pos.Value.Y < -5)
                             {
                                 goToCoordinate(m_startPosition, 1);
                                 WaitSimulatorStep();
